5ab822b41fb3838cfd641d3db0189bfe26c44f1c
Alejandro <alejandrobrb210501@gmail.com>
2023-05-22 13:27:09 +0200

    Handle errors when create a new expense

--- expensestracker/src/app/components/dialogs/create-expense-dialog/create-expense-dialog.component.ts
+++ expensestracker/src/app/components/dialogs/create-expense-dialog/create-expense-dialog.component.ts
@@ -1,7 +1,8 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 import { ApiService } from 'src/app/services/api.service';
+import { ErrorDialogComponent } from '../error-dialog/error-dialog.component';
 import { Expense } from 'src/app/models/expense';
 
 export interface ExpenseDialogData {
@@ -21,10 +22,15 @@ export class CreateExpenseDialogComponent {
     public dialogRef: MatDialogRef<CreateExpenseDialogComponent>,
     private apiService: ApiService,
     @Inject(MAT_DIALOG_DATA) public data: ExpenseDialogData,
+    private dialog: MatDialog,
   ) {}
 
   onCreate() {
-    this.apiService.createExpense(this.data.spaceId, this.expenseDescription, this.expenseCost)
+    this.apiService.createExpense(
+        this.data.spaceId, 
+        this.expenseDescription, 
+        this.expenseCost,
+        (err) => ErrorDialogComponent.openDialog(this.dialog, err.error))
       .subscribe(result => this.dialogRef.close(result as Expense));
   }
 }
